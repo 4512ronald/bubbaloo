a45c552a8906b9538a2753df8c3304c06ed325f4
Ronald Alberto Restrepo Castrillón <ronald.restrepo@quind.io>
2023-12-20 23:46:22 -0500

    feat: Se mejora la clase orquestadora de flujos

--- src/bubbaloo/services/pipeline/orchestration.py
+++ src/bubbaloo/services/pipeline/orchestration.py
@@ -2,32 +2,31 @@ import os
 import inspect
 import importlib
 import re
+from typing import Dict, Any, List, Tuple, Type
 
 from bubbaloo.pipeline.stages.extract import Extract
 from bubbaloo.pipeline.stages.load import Load
 from bubbaloo.pipeline.stages.transform import Transform
 from bubbaloo.pipeline.pipeline import Pipeline
 from bubbaloo.services.pipeline import Config
+from bubbaloo.utils.interfaces.pipeline_logger import ILogger
 
 
-class PipelineOrchestrator:
+class Orchestrator:
 
-    def __init__(self, path_to_flows: str | object, conf: Config, flows_dir_name: str | None = None):
-        # TODO Manejar los parámetros con kwargs
-        if inspect.ismodule(path_to_flows):
-            self.path_to_flows: str = path_to_flows.__path__[0]
-            self.flows_dir_name: str = path_to_flows.__name__
-        else:
-            self.path_to_flows = path_to_flows
-            self.flows_dir_name = flows_dir_name
-
-        self.conf = conf
-        self.pipeline_list = []
-        self.pipeline_stage_types = (Transform, Load, Extract)
+    def __init__(self, path_to_flows: str | object, flows_to_execute: List[str] | None = None, **kwargs):
+        self._path_to_flows: str = path_to_flows.__path__[0] if inspect.ismodule(path_to_flows) else path_to_flows
+        self._flows_to_execute: List[str] = flows_to_execute if flows_to_execute is not None else []
+        self._flows_dir_name: str = self._path_to_flows.split("/")[-1]
+        self._params: Dict[str, Any] = kwargs
+        self._conf: Config = self._params.get("conf")
+        self._logger: ILogger = self._params.get("logger")
+        self._pipeline_list: List[Tuple[str, Pipeline]] = []
+        self._pipeline_stage_types: Tuple[Type[Transform], Type[Load], Type[Extract]] = (Transform, Load, Extract)
 
     @staticmethod
-    def get_module_names_from_directory(directory_path):
-        module_names = []
+    def _get_module_names_from_directory(directory_path: str) -> List[str]:
+        module_names: List[str] = []
         with os.scandir(directory_path) as entries:
             for entry in entries:
                 match = re.match(r"(\w+)\.py", entry.name)
@@ -37,36 +36,44 @@ class PipelineOrchestrator:
                 module_names.append(module_name)
         return module_names
 
-    def get_pipeline_phases_from_directory(self, flow_name):
-        pipeline_phases = []
+    def _get_pipeline_phases_from_directory(self, flow_name: str) -> list[tuple[str, Transform | Load | Extract]]:
+        pipeline_phases: list[tuple[str, Transform | Load | Extract]] = []
 
-        for stage_type in self.get_module_names_from_directory(os.path.join(self.path_to_flows, flow_name)):
-            module_path = f"{self.flows_dir_name}.{flow_name}.{stage_type}"
+        for stage_type in self._get_module_names_from_directory(os.path.join(self._path_to_flows, flow_name)):
+            module_path = f"{self._flows_dir_name}.{flow_name}.{stage_type}"
 
             try:
                 module = importlib.import_module(module_path)
-
-                phase = self.get_pipeline_phase(module)
-
+                phase = self._get_pipeline_phase(module)
                 pipeline_phases.extend(phase)
-
             except ModuleNotFoundError as e:
-                # TODO log error en vez de print
-                print(f"Cannot import the module {module_path}: {e}")
+                self._logger.error(f"Cannot import the module {module_path}: {e}")
         return pipeline_phases
 
-    def get_pipeline_phase(self, module):
-        pipeline_phase = []
+    def _get_pipeline_phase(self, module) -> List[Tuple[str, Transform | Load | Extract]]:
+        pipeline_phase: list[tuple[str, Transform | Load | Extract]] = []
         for name, obj in inspect.getmembers(module):
-            if inspect.isclass(obj) and issubclass(obj, self.pipeline_stage_types) and obj not in self.pipeline_stage_types:
+            if (
+                    inspect.isclass(obj)
+                    and issubclass(obj, self._pipeline_stage_types)
+                    and obj not in self._pipeline_stage_types
+            ):
                 pipeline_phase.append((name, obj()))
         return pipeline_phase
 
-    def orchestrate_pipelines(self):
-        for flow_name in os.listdir(self.path_to_flows):
-            pipeline_phases = self.get_pipeline_phases_from_directory(flow_name)
-            pipeline = Pipeline(conf=self.conf)
-            pipeline.stages(pipeline_phases)
-            self.pipeline_list.append((flow_name, pipeline))
+    def _get_flows_to_execute(self):
+        for flow_name in os.listdir(self._path_to_flows):
+            if not self._flows_to_execute or flow_name in self._flows_to_execute:
+                pipeline_phases = self._get_pipeline_phases_from_directory(flow_name)
+                pipeline = Pipeline(**self._params)
+                pipeline.stages(pipeline_phases)
+                self._pipeline_list.append((flow_name, pipeline))
 
-    # TODO Ejecutar pipelines
+    def execute(self):
+        self._get_flows_to_execute()
+        for flow_name, pipeline in self._pipeline_list:
+            self._logger.info(f"Executing pipeline for flow: {flow_name}")
+            try:
+                pipeline.execute()
+            except Exception as e:
+                self._logger.error(f"Error executing pipeline for flow {flow_name}: {e}")
