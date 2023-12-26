b0dd77ba03463339a5e9e25db478ed3c1a84a8b2
Ronald Alberto Restrepo Castrillón <ronald.restrepo@quind.io>
2023-12-26 09:52:41 -0500

    docs: Se agregan docstrings

--- src/bubbaloo/services/validation/parquet.py
+++ src/bubbaloo/services/validation/parquet.py
@@ -24,8 +24,20 @@ from bubbaloo.utils.interfaces.services_validation import IValidation
 
 
 class Parquet(IValidation):
+    """
+    A class that implements the IValidation interface for validating Parquet files.
+
+    This class includes methods for validating the schema and integrity of Parquet files, handling invalid files,
+    and managing errors during validation.
+    """
 
     def __init__(self, **kwargs) -> None:
+        """
+        Initializes the Parquet validator with various parameters.
+
+        Args: kwargs (dict): A dictionary of parameters including objects to validate, schemas, Spark session,
+        logger, storage client, error context, and error path.
+        """
         self._params: Dict[str, Any] = validate_params(kwargs)
         self._objects_to_validate: List[str] = self._params.get("objects_to_validate")
         self._spark_schema: StructType = self._params.get("spark_schema")
@@ -37,6 +49,16 @@ class Parquet(IValidation):
         self._error_path: str = self._params.get("error_path")
 
     def _get_invalid_blobs(self, file_paths: List[str]) -> Generator[Dict[str, str], None, None]:
+        """
+        Identifies invalid blobs in the provided file paths.
+
+        Args:
+            file_paths (List[str]): A list of file paths to validate.
+
+        Yields:
+            Generator[Dict[str, str], None, None]: A generator yielding dictionaries with error details for each invalid
+            blob.
+        """
         for file_path in file_paths:
             error = self._validate_file(file_path)
             if error is not None:
@@ -44,12 +66,29 @@ class Parquet(IValidation):
 
     @staticmethod
     def _verify_schema(required_schema: pa.Schema, input_schema: pa.Schema) -> None:
+        """
+        Verifies if the input schema matches the required schema.
 
+        Args:
+            required_schema (pa.Schema): The required schema to validate against.
+            input_schema (pa.Schema): The schema of the input data to validate.
+
+        Raises:
+            DataFrameSchemaError: If there are differences between the required and input schemas.
+        """
         if differences := compare_schemas(required_schema, input_schema):
             raise DataFrameSchemaError(str(differences))
 
     def _verify_parquet_file(self, file_path: str) -> None:
+        """
+        Validates a single Parquet file for format and readability.
 
+        Args:
+            file_path (str): The path of the file to validate.
+
+        Raises:
+            CorruptedPathError: If the file is not a Parquet file or cannot be read properly.
+        """
         if file_path.endswith("/_SUCCESS") or file_path.endswith("/"):
             return
         if not file_path.endswith(".parquet"):
@@ -63,6 +102,12 @@ class Parquet(IValidation):
             raise CorruptedPathError(error_message) from error
 
     def _move_invalid_files(self, invalid_blobs: List[Dict[str, str]]) -> None:
+        """
+        Moves invalid files to a designated error path.
+
+        Args:
+            invalid_blobs (List[Dict[str, str]]): A list of invalid blobs containing file paths and error messages.
+        """
         self._logger.error(
             "Some corrupted files found: \n"
             f"{json.dumps(self._error_context.errors['dataProcessing'])} \n"
@@ -79,7 +124,12 @@ class Parquet(IValidation):
         )
 
     def _handle_invalid_files(self, not_valid_blob: List[Dict[str, str]]) -> None:
+        """
+        Handles invalid files by sorting and grouping them based on their error messages.
 
+        Args:
+            not_valid_blob (List[Dict[str, str]]): A list of invalid blobs containing file paths and error messages.
+        """
         sorted_list = sorted(not_valid_blob, key=lambda item: item["error"])
         validation_errors = [
             {"message": key, "files": [item["path"] for item in items]}
@@ -89,6 +139,15 @@ class Parquet(IValidation):
         self._error_context.errors["dataProcessing"] = str(validation_errors)
 
     def _get_schema_from_temp_copy(self, file_path: str) -> pa.Schema:
+        """
+        Retrieves the PyArrow schema from a temporary copy of the file.
+
+        Args:
+            file_path (str): The path of the file to retrieve the schema from.
+
+        Returns:
+            pa.Schema: The schema of the file.
+        """
         with tempfile.TemporaryDirectory(prefix=os.path.basename(__file__)) as temp_dir:
             temp_file_path = os.path.join(temp_dir, os.path.basename(file_path))
             self._storage_client.copy([file_path], temp_dir)
@@ -96,7 +155,15 @@ class Parquet(IValidation):
             return get_pyarrow_schema(temp_file_path)
 
     def _validate_file(self, path: str) -> Dict[str, str] | None:
+        """
+        Validates a single file for format, readability, and schema.
 
+        Args:
+            path (str): The path of the file to validate.
+
+        Returns:
+            Dict[str, str] | None: A dictionary containing error details if the file is invalid, None otherwise.
+        """
         try:
             self._verify_parquet_file(path)
             pa_schema = self._get_schema_from_temp_copy(path)
@@ -116,7 +183,11 @@ class Parquet(IValidation):
             return {"error": identify_error(error), "path": path}
 
     def execute(self) -> None:
+        """
+        Executes the validation process for the set of files to be validated.
 
+        Logs information about the validation process and handles any invalid files found during validation.
+        """
         self._logger.info("Checking integrity of the data...")
 
         if invalid_blobs := list(self._get_invalid_blobs(self._objects_to_validate)):
